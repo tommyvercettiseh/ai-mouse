a96749f2330644442e057ad42c18f54dcd330164
tommyvercettiseh <tommyvercettiseh@gmail.com>
2026-07-24 16:19:33 +0200

    Add package initialization

---
+++ ai_mouse_hub/__init__.py
@@ -0,0 +1,5 @@
+"""Standalone AI Mouse Profile Hub."""
+
+from .core import build_master_profile, list_sessions, run_stress_test
+
+__all__ = ["build_master_profile", "list_sessions", "run_stress_test"]
